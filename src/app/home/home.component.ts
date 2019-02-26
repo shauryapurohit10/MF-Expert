acf9da98fc4562dc4b2cb49739999c1e5964fe4c
Shaurya Purohit <shaurya.purohit@sls.ads.valuelabs.net>
2019-02-26 18:14:02 +0530

    1.2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,10 +7,59 @@ import { Router } from '@angular/router';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+/*
+id:any = '';
+name:any = '';
+cnumber:any = '';
 
-  constructor(private router : Router) { }
+Employee:any [] = [];*/
+employees:any;
+
+
+
+// addRow() {
+//     //prompt("Checking the control!");
+//     this.employees.push({
+//         id: this.id,
+//         name: this.name,
+//         cnumber: this.cnumber
+//     });
+// }
+
+// deleteEmployee(index) {
+//     this.employees.splice(index, 1);
+// }
+
+// editEmployee(index) {
+
+//     //code for editing
+
+// }
+
+  constructor(private router : Router) {
+
+    this.employees = [
+        {
+            id: '1',
+            name: 'ABC',
+            cnumber: '1234567890'    
+        },
+        {
+            id: '2',
+            name: 'DEF',
+            cnumber: '0987654321' 
+        },
+        {
+            id: '3',
+            name: 'GHI',
+            cnumber: '1122334455'
+        }
+    ];
+   }
 
   ngOnInit() {
+      //emp: Employee[] = [];
+
   }
 
 }
